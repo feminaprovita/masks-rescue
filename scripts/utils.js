8d59980fd22302942237d318e20988e3fa9ced84
lagoda <10778958+lagoda@users.noreply.github.com>
2019-02-23 12:49:41 -0500

    moving read-write functions into utils

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,9 +1,21 @@
+const fs = require('fs')
+
 const sortPostsByDate = (a,b) => {
   let d1 = new Date(a.published)
   let d2 = new Date(b.published)
   return d1.getTime() === d2.getTime() ? 0 : (d1.getTime() > d2.getTime() ? -1 : 1)
 }
 
+const readJsonFile = (filename) => {
+  return JSON.parse(fs.readFileSync(filename, 'utf8')) 
+}
+
+const writeJsonFile = (filename, objectToWrite) => {
+  fs.writeFileSync(filename, JSON.stringify(objectToWrite, undefined, 2))
+}
+
 module.exports = {
-  sortPostsByDate
+  sortPostsByDate,
+  readJsonFile,
+  writeJsonFile
 }
